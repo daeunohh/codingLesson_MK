be4a47a40a40213c4ec28b24fdf5cb234be0f2ba
Da-Eun Oh <76278018+daeunohh@users.noreply.github.com>
2025-03-13 02:19:51 +0000

    codeup 6098

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # codingLesson_MK
+
+1. 기초 문법 풀이
+    https://school.programmers.co.kr/learn/courses/2/2-%EB%AC%B4%EB%A3%8C-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%9E%85%EB%AC%B8
+2.  
---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+def Print(array) :
+    for line in array :
+        print(" ".join(map(str, line)))
+#####
+
+array = []
+for i in range(10):
+    array.append(list(map(int, input().split())))
+
+n = 10
+i = j = 1
+
+#####
+while True :
+    if array[i][j] == 2 : # On Food
+        array[i][j] = 9
+        Print(array)
+        break
+    else :
+        array[i][j] = 9
+    
+    # go right
+    if array[i][j + 1] == 0 or array[i][j + 1] == 2 :
+        j += 1
+    # go down
+    elif array[i + 1][j] == 0 or array[i + 1][j] == 2 :
+        i += 1
+    # dont move
+    else :
+        Print(array)
+        break
+    
